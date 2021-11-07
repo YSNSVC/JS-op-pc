4861b17d442fe708fd1e4221099b287f1219a775
YSNSVC <89729328+YSNSVC@users.noreply.github.com>
2021-11-07 15:48:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//This is a large sum
+//Run your code with Node. Do you see the outcome of your comment in the console? If so, why? If not, why not? Ja, het is zichtbaar omdat js al aan het draaien is.
+/*console.log(1230941 * 1823794)
+
+console.log(637263 / 54)*/
+
+//Run your code again with Node. Can you still see the result of the sum in the console? Why/why not? Het is niet meer zichtbaar, omdat de code binnen het comment valt.
+
